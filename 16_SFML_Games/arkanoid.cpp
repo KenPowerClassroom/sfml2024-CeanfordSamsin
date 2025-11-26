5623b9ab78142e98d6878b4726c8da62861beb16
C00310517 <C00310517@setu.ie>
2025-11-26 14:42:40 +0000

    Refactor: Extract method

--- 16_SFML_Games/arkanoid.cpp
+++ 16_SFML_Games/arkanoid.cpp
@@ -20,6 +20,47 @@ const float PADDLE_START_X = 300.f;
 const float PADDLE_START_Y = 440.f;
 const float PADDLE_SPEED = 6.f;
 
+// Handle ball–block collisions (affects dx or dy)
+void handleBallBlockCollision(float ballX, float ballY, float& ballSpeed,
+    Sprite blockSprites[], int blockCount)
+{
+    FloatRect ballRect(ballX + 3, ballY + 3, 6, 6);
+
+    for (int i = 0; i < blockCount; i++)
+    {
+        if (ballRect.intersects(blockSprites[i].getGlobalBounds()))
+        {
+            blockSprites[i].setPosition(-100, 0);
+            ballSpeed = -ballSpeed;
+            return; // only one bounce per frame
+        }
+    }
+}
+
+// handle bouncing from window borders
+void handleWallCollision(float ballX, float ballY, float& ballDx, float& ballDy)
+{
+    if (ballX < 0 || ballX > WINDOW_WIDTH)  ballDx = -ballDx;
+    if (ballY < 0 || ballY > WINDOW_HEIGHT) ballDy = -ballDy;
+}
+
+// handle user input for paddle movement
+void updatePaddleMovement(Sprite& paddleSprite)
+{
+    if (Keyboard::isKeyPressed(Keyboard::Right))
+        paddleSprite.move(PADDLE_SPEED, 0);
+
+    if (Keyboard::isKeyPressed(Keyboard::Left))
+        paddleSprite.move(-PADDLE_SPEED, 0);
+}
+
+// handle paddle–ball collision
+void handlePaddleCollision(float ballX, float ballY, float& ballDy, const Sprite& paddleSprite)
+{
+    if (FloatRect(ballX, ballY, BALL_SIZE, BALL_SIZE).intersects(paddleSprite.getGlobalBounds()))
+        ballDy = -(rand() % 5 + 2);
+}
+
 int arkanoid()
 {
     srand(time(0));
@@ -59,28 +100,21 @@ int arkanoid()
                 window.close();
         }
 
+        // ball movement
         ballX += ballDx;
-        for (int i = 0; i < blockCount; i++)
-            if (FloatRect(ballX + 3, ballY + 3, 6, 6).intersects(blockSprites[i].getGlobalBounds()))
-            {
-                blockSprites[i].setPosition(-100, 0); ballDx = -ballDx;
-            }
+        handleBallBlockCollision(ballX, ballY, ballDx, blockSprites, blockCount);
 
         ballY += ballDy;
-        for (int i = 0; i < blockCount; i++)
-            if (FloatRect(ballX + 3, ballY + 3, 6, 6).intersects(blockSprites[i].getGlobalBounds()))
-            {
-                blockSprites[i].setPosition(-100, 0); ballDy = -ballDy;
-            }
+        handleBallBlockCollision(ballX, ballY, ballDy, blockSprites, blockCount);
 
-        if (ballX<0 || ballX>WINDOW_WIDTH)  ballDx = -ballDx;
-        if (ballY<0 || ballY>WINDOW_HEIGHT) ballDy = -ballDy;
+        // wall collision
+        handleWallCollision(ballX, ballY, ballDx, ballDy);
 
-        if (Keyboard::isKeyPressed(Keyboard::Right)) paddleSprite.move(PADDLE_SPEED, 0);
-        if (Keyboard::isKeyPressed(Keyboard::Left))  paddleSprite.move(-PADDLE_SPEED, 0);
+        // paddle movement
+        updatePaddleMovement(paddleSprite);
 
-        if (FloatRect(ballX, ballY, BALL_SIZE, BALL_SIZE).intersects(paddleSprite.getGlobalBounds()))
-            ballDy = -(rand() % 5 + 2);
+        //paddle collision
+        handlePaddleCollision(ballX, ballY, ballDy, paddleSprite);
 
         ballSprite.setPosition(ballX, ballY);
 
